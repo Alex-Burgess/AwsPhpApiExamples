3631f8aebda7a944496f61b2aeabccb4cce79e87
Alex-Burgess <burgess.alexander@gmail.com>
2018-07-09 17:44:13 +0100

    cf template updated to create s3 buckets. Bucket updated in php files to match.

--- list-bucket-objects.php
+++ list-bucket-objects.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 require 'classes/s3-connection.php';     // Provides S3 connection in variable $s3 or $s3_connect_error
 
-$GLOBALS['bucket'] = 'froome-dog';
+$GLOBALS['bucket'] = 'php-aws-examples';
 ?>
 <!doctype html>
 <html lang="en">
