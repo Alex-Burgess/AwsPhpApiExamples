bcfb4fe6c030b9512d3341e4f64bba6ff42231b3
Alex-Burgess <burgess.alexander@gmail.com>
2018-07-10 14:19:51 +0100

    Updated s3-connection.php class, so that local php deployments don't assume role and authenticate with local credentials.

--- classes/s3-connection.php
+++ classes/s3-connection.php
@@ -7,16 +7,20 @@ use Aws\S3\Exception\S3Exception;
 use Aws\Credentials\AssumeRoleCredentialProvider;
 
 try {
-  $assumeRoleCredentials = new AssumeRoleCredentialProvider([
-    'client' => new StsClient([
-        'region' => $app_configuration['region'],
-        'version' => '2011-06-15'
-    ]),
-    'assume_role_params' => [
-        'RoleArn' => $app_configuration['roleArn'], // $app_configuration comes from application-configuration.php
-        'RoleSessionName' => $app_configuration['roleSessionName'], // $app_configuration comes from application-configuration.php
-    ]
-  ]);
+   // If the PHP_APP_ENV environment variable is set, the application is deployed to elastic beanstalk.
+   // If no environment variable is set, the application is being used locally and will authenticate using own credentials.
+   if ($_SERVER['PHP_APP_ENV']) {
+      $assumeRoleCredentials = new AssumeRoleCredentialProvider([
+        'client' => new StsClient([
+            'region' => $app_configuration['region'],
+            'version' => '2011-06-15'
+        ]),
+        'assume_role_params' => [
+            'RoleArn' => $app_configuration['roleArn'], // $app_configuration comes from application-configuration.php
+            'RoleSessionName' => $app_configuration['roleSessionName'], // $app_configuration comes from application-configuration.php
+        ]
+      ]);
+   }
 
   $s3 = new S3Client([
       'region' => $app_configuration['region'],
